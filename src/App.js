161abecc64e7406b693a16ff29eae985b76d74cd
DarQxxx <norbertju@gmail.com>
2021-10-11 23:13:34 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+export default function App() {
+  const [heightOfMessagedBoard, setHeightOfMessagedBoard] = useState("0");
+  const [heightOfMessageField, setHeightOfMessageField] = useState("0")
+  const messageInput = useRef();
+useEffect(() => {
+  //bierzemy wysokość diva, będącego rodzicem inputa
+  let messagesHeight = document.querySelector(".msg-field").offsetHeight;
+  //wysokość diva będącego tłem, na którym wyświetlają się wiadomości (wysokość ekranu - wysokość diva z inputem)
+  let messageBoardHeight = window.innerHeight - messagesHeight;
+  //łapiemy za div, który jest tłem wysłanych wcześniej wiadomości
+  let messagedBoard = document.querySelector(".back");
+  //ustawiamy wysokość tła wysłanych wcześniej wiadomości na to co obliczyliśmy
+  messagedBoard.style.height = messageBoardHeight + "px";
+  // zapisujemy wysokość tła wiadomości w state
+  setHeightOfMessagedBoard(messageBoardHeight);
+ // zapisujemy wysokość rodzica inputa w state
+ setHeightOfMessageField(messagesHeight);
+
+}, [])
+
+function changeHeight(event){
+  let messagesHeight = document.querySelector(".msg-field").offsetHeight;
+  console.log(messagesHeight);
+  console.log(heightOfMessageField)
+  if (messagesHeight != heightOfMessageField){
+  //wysokość diva będącego tłem, na którym wyświetlają się wiadomości (wysokość ekranu - wysokość diva z inputem)
+  let messageBoardHeight = window.innerHeight - messagesHeight;
+  //łapiemy za div, który jest tłem wysłanych wcześniej wiadomości
+  let messagedBoard = document.querySelector(".back");
+  //ustawiamy wysokość tła wysłanych wcześniej wiadomości na to co obliczyliśmy
+  messagedBoard.style.height = messageBoardHeight + "px";
+  // zapisujemy wysokość tła wiadomości w state
+  setHeightOfMessagedBoard(messageBoardHeight);
+ // zapisujemy wysokość rodzica inputa w state
+ setHeightOfMessageField(messagesHeight);
+  } 
+
 }
 
-export default App;
+function test(){
+  console.log("test");
+}
+
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className=" back text-center col-12">
+            Rozmawiaj ze swoimi znajomymi
+          </div>
+
+          
+        </div>
+        <div className="row">
+        <div className=" msg-field text-center col-12 ">
+              <form>
+                <div contentEditable="true" className="msg-field--textarea col-11 msg-field--textarea-clear" ref={messageInput} onKeyPress={changeHeight}></div>
+              </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
---
+++ src/scripts.js
@@ -0,0 +1,9 @@
+
+
+function setSentMessagesHieght(){ 
+    var messagesHeight = document.querySelector(".msg-field");
+    console.log(messagesHeight);
+//let height =  screen.height - 
+}
+
+setSentMessagesHieght();
