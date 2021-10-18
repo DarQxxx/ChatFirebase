b7b1a92019c0da47e291e2f3d0f882e660081ed5
DarQxxx <norbertju@gmail.com>
2021-10-18 22:13:42 +0200

    Restyle login form

--- src/page/LogIn.js
+++ src/page/LogIn.js
@@ -6,11 +6,12 @@ export default function LogIn() {
         <div className="container">
             <div class="login-box">
             <form className="login-form d-flex flex-column text-center align-items-center">
-                <label for="email" className="login-labels pb-2">Wpisz swój e-mail</label>
-                <input type="email" id="email" className="mb-4 w-50" ></input>
-                <label for="email" id="passwd" className="login-labels pb-2">Wpisz swoje hasło</label>
-                <input type="password" id="passwd" className="mb-4 w-50" ></input>
-               <Link to="/chat"> <div className="login-button ">Zaloguj</div></Link>
+                <label for="email" className="login-labels pb-2 w-75">E-mail</label>
+                <input type="email" id="email" className="mb-2 w-75 login-inputs" ></input>
+                <label for="email" id="passwd" className="login-labels pb-2 w-75">Hasło</label>
+                <input type="password" id="passwd" className="mb-4 w-75 login-inputs" ></input>
+               <Link to="/chat"> <div className="login-button">Zaloguj</div></Link>
+               <div className="login-register w-75 mt-1">Nie posiadasz konta? <Link to="/chat"><span className="login-register--clickHere">Zarejestruj się</span></Link></div>
             </form>
             </div>
         </div>
